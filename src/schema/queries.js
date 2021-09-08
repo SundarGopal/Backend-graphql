c4ef956e7b892b4ad515cab3d9de3b3fdd40757e
umesh <umeshyarapurpose@gmail.com>
2021-09-08 16:27:33 +0530

    leaves queries working fine, but attendance var declare error

--- src/schema/queries.js
+++ src/schema/queries.js
@@ -2,6 +2,7 @@ const { GraphQLObjectType } = require('graphql')
 const employeeQueries = require('../model/employee/queries')
 const salariesQueries = require('../model/salaries/queries')
 const leavesQueries = require('../model/leaves/queries')
+const attendanceQueries = require('../model/attendance/queries')
 
 module.exports = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -9,6 +10,7 @@ module.exports = new GraphQLObjectType({
         employer: employeeQueries.employer,
         employee: employeeQueries.employee,
         salaries: salariesQueries.salaries,
-        leaves: leavesQueries.leaves
+        leaves: leavesQueries.leaves,
+        attendance:attendanceQueries.attendance
     }
 })
